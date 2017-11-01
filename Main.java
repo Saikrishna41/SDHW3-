e6c96639d44e12e08218d8abcefefe2eab96cc70
Saikrishna41 <saai.krrishna@gmail.com>
2017-10-31 19:17:06 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,126 +1,120 @@
-package main.java.SDHW3rd;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import java.util.Arrays;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
 
 
-import org.apache.commons.cli.*;
+public class Main implements Comparable{
 
-public class Main implements Comparable {
+	CommandLine cmd = null;
+	String type;
+	int middle,iKey;
+	Integer[] iList;
+	String aList[],key;
+	boolean result;
+	public Main(String[] args) {
+			if(validateOptions(args)) {
+				   validate();
+			}
+		}
 
+	public int compareTo(Object o) {
+		return 0;
+	}	
+	
+	
+	private boolean binSearch(Comparable[] aList, Comparable key) {
+		int low=0;
+		int high=aList.length-1;
+		
+		while(low<=high) {
+			
+			int middle=(low+high)/2;
+			
+			if(aList[middle].equals(key)) {
+				output=true;
+			}
+			if(aList[middle].compareTo(key)<0) {
+				low=middle+1;
+			}
+			else {
+			high=middle-1;
+			}
+		}
+		
+		return result;
+	}
+	
+	
+		
 
-    public static boolean binSearch(Comparable[] aList, Comparable key) {
-
-
-        int low = 0;
-        int high = aList.length - 1;
-        int arr_size = aList.length;
-        int s;
-        int o[] = new int[100];
-        int res = -1;
-        int l;
-        double temp = 0.0;
-        for (int i = 0; i < arr_size; i++) {
-
-            o[i] = i;
-
-        }
-        for (int i = 0; i < arr_size - 1; i++)
-            for (int j = 0; j < arr_size - i - 1; j++)
-                if (aList[j].compareTo(aList[j + 1]) < 0) {
-                    // swapping the vals of temp and arr[i]
-                    temp = Double.parseDouble(aList[j].toString());
-                    aList[j] = aList[j + 1];
-                    aList[j + 1] = temp;
-                    l = o[j];
-                    o[j] = o[j + 1];
-                    o[j + 1] = l;
-
-                }
-
-        while (low <= high) {
-            int middle = (high + low) / 2;
-
-
-            int checkVal = aList[middle].compareTo(key);
-
-            if (checkVal == 0) {
-
-                res = middle;
-
-                high = middle - 1;
-
-
-            } else if (checkVal == -1) {
-
-                low = middle + 1;
-
-            } else if (checkVal == 1) {
-
-                high = middle - 1;
-            }
-
-        }
-
-
-        if (res >= 0) {
-            System.out.println("output:" + o[res]);
-        } else {
-            System.out.println("The key not found!");
-
-        }
-        return false;
-    }
-
-    public int compareTo(Object o) {
-        return 0;
-    }
-
-    public static void main(String[] args) {
-
-        double aList[];
-
-        double k = 0;
-        double keyIndex = 0;
-
-        Options opt = new Options();
-        Option typeOp = Option.builder("t").longOpt("type").desc("specifying the types of input:“i” for integer and “s” for string").hasArg().numberOfArgs(1).required(true).build();
-        opt.addOption(typeOp);
-        Option keyOp = Option.builder("k").longOpt("key").desc("specifying the value of given key for searching").hasArg().numberOfArgs(1).required(true).build();
-        opt.addOption(keyOp);
-        Option listOp = Option.builder("l").longOpt("list").hasArg().numberOfArgs(Option.UNLIMITED_VALUES).desc("specifies the sorted list of integers or strings").valueSeparator(' ').required(true).build();
-        opt.addOption(listOp);
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = null;
-        try {
-            cmd = parser.parse(opt, args, true);
-        } catch (ParseException e) {
+	private boolean validateOptions(String[] args) {
+        
+		Options options = new Options();
+		options.addOption(Option.builder("type").desc("Specifying type of elements").longOpt("type").numberOfArgs(1).build());
+		options.addOption(Option.builder("key").desc("Specifying key to search").longOpt("key").numberOfArgs(1).build());
+		options.addOption(Option.builder("list").desc("Specifying the list of elements").longOpt("list").numberOfArgs(Option.UNLIMITED_VALUES).build());
+		CommandLineParser parser = new DefaultParser();
+		try {
+			cmd=parser.parse(options,args);
+		}
+		catch(Exception e) { 
             e.printStackTrace();
-            System.exit(0);
+            return false;
         }
+        
+        
+		type=cmd.getOptionValue("type");
+		key=cmd.getOptionValue("key");
+		aList =cmd.getOptionValues("list");
+		
+		return true;
+	}
+	
+	//Method to validate
+	private void validate() {
+	
 
-        Main ml = new Main();
+		if(type.equals("i"))
+		    {
+			try {
+		        iKey = Integer.parseInt(key);
+		        iList = new Integer[aList.length];
+		        for (int i = 0; i < aList.length; i++)
+		            iList[i] = Integer.parseInt(aList[i]);
+		        }
+				catch(Exception e) {System.out.println("Please enter integers to sort");}
+			
+				Arrays.sort(iList);
+		        result = binSearch(iList,iKey);
+		        printOutput();
+		    }
+		    else if(type.equals("s"))
+		    {
+		    	Arrays.sort(aList);
+		        result = binSearch(aList,key);
+		        printOutput();
+		    }
+		    else {System.out.println("Please enter a valid argument");}
+		}
+			
 
-        boolean isFound = false;
-        String type = cmd.getOptionValue("type");
-        String key = cmd.getOptionValue("key");
-        String[] list = cmd.getOptionValues("list");
+	private void printOutput() {
+		System.out.print("Output:");
+		if(result) {
+			System.out.println("1");	
+		}
+		else {
+			System.out.println("0");
+		}
+	}
 
-        if (type.equals("i")) {
-            int keyAsInt = Integer.parseInt(key);
-            Integer[] listAsInt = new Integer[list.length];
-            for (int i = 0; i < listAsInt.length; i++)
-                listAsInt[i] = Integer.parseInt(list[i]);
-            isFound = ml.binSearch(listAsInt, keyAsInt);
-        } else if (type.equals("s")) {
-            isFound = ml.binSearch(list, key);
-        } else
-            System.out.println("\nInvalid arguments.");
+	public static void main(String[] args) {
+		
+		new Main(args);
+	}
 
-        if (isFound == true)
-            System.out.println("1");
-        else
-            System.out.println("0");
-    }
-
+	
 }
-
-
